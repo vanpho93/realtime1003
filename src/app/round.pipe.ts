c3d692c37f58d2bbf8d252644e553feb178cea83
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-26 19:01:45 +0700

    xong

--- src/app/round.pipe.ts
+++ src/app/round.pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RoundPipe implements PipeTransform {
 
-  transform(value: number, isCeil: boolean): number {
+  transform(value: number, isCeil: boolean, addition: number = 0): number {
     if (isCeil) {
-      return Math.ceil(value);
+      return Math.ceil(value + addition);
     }
-    return Math.floor(value);
+    return Math.floor(value + addition);
   }
 
 }
